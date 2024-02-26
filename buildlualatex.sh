b3795987e5da97874b8e41fbc7d6c6a1f4fcf536
junekanenjoh <junekanenjoh@smail.informatik.hs-bremerhaven.de>
2024-02-27 00:06:22 +0100

    Anfang mit zwei Containern

--- buildlualatex.sh
+++ buildlualatex.sh
@@ -3,4 +3,6 @@
 #lualatex poster.tex
 latexmk -pdf -lualatex -shell-escape -interaction=nonstopmode -f -outdir=log hauptdatei.tex
 
-#mv log/hauptdatei.pdf ./ 
+
+mv log/hauptdatei.pdf ./ 
+cp hauptdatei.pdf /var/www/html/$USER/
